3813a95ca225dcdc285757387575bbcc3b238dd2
floiwebsite <renren.gerald@gmail.com>
2024-05-23 11:09:09 +0800

    working system

---
+++ Queu Numbers/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleTCP;
+
+namespace Queu_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ipAddress = "192.168.4.109";
+        private string portNumber = "8910";
+        private string c1 = "";
+        SimpleTcpClient client;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            passData("add");
+        }
+
+        private async void passData(string nk)
+        {
+            try
+            {
+                if (int.TryParse(label1.Text, out int number))
+                {
+                    label1.Text = "***";
+                    await Task.Delay(1000);
+
+                    int incrementedNumber = number;
+                    if (nk == "add")
+                    {
+                        incrementedNumber = number + 1;
+                    }
+                    else if (nk == "sub")
+                    {
+                        incrementedNumber = number - 1;
+                    }
+
+                    scanRadio();
+
+                    string message = $"{c1}:" + incrementedNumber.ToString();
+
+                    client.WriteLineAndGetReply(message, TimeSpan.FromSeconds(2));
+                    await Task.Delay(5000);
+                    label1.Text = incrementedNumber.ToString();
+                }
+                else
+                {
+                    textBox1.Text += "Error: Invalid number format in label1" + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text += $"Error: {ex.Message}" + Environment.NewLine;
+            }
+        }
+
+
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            try { 
+            string message = e.MessageString.Trim((char)client.Delimiter);
+            textBox1.Invoke((MethodInvoker)delegate
+            {
+                textBox1.AppendText(message + Environment.NewLine);
+            });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            
+        }
+
+        private void startConnect()
+        {
+            try
+            {
+                int port = Convert.ToInt32(portNumber);
+                client.Connect(ipAddress, port);
+            }
+            catch
+            {
+                MessageBox.Show($"Error: Queuing Main System is currently not running" + Environment.NewLine);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+                radioButton1.Checked = true;
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+            startConnect();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            passData("sub");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            passData("");
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanRadio ()
+        {
+
+            TableLayoutPanel tableLayoutPanel3 = null;
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is TableLayoutPanel panel && control.Name == "tableLayoutPanel3")
+                {
+                    tableLayoutPanel3 = panel;
+                    break;
+                }
+            }
+            if (tableLayoutPanel3 != null)
+            {
+                foreach (Control control in tableLayoutPanel3.Controls)
+                {
+                    if (control is RadioButton radioButton && radioButton.Checked)
+                    {
+                        string selectedText = radioButton.Text;
+                        c1 = selectedText;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
--- Queuing System/Main.cs
+++ Queuing System/Main.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
+using SimpleTCP;
 
 namespace Queuing_System
 {
@@ -18,13 +19,114 @@ namespace Queuing_System
             InitializeComponent();
         }
 
+        private string ipAddress = "192.168.4.109";
+        private string portNumber = "8910";
+        SimpleTcpServer server;
+
         private void Main_Load(object sender, EventArgs e)
         {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+
             axWindowsMediaPlayer1.URL = @"C:\Users\Administrator\Downloads\Video\LeAnn Rimes - I Need You (Official Music Video).mp4";
             axWindowsMediaPlayer1.uiMode = "none";
             axWindowsMediaPlayer1.settings.autoStart = true;
             axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            clearButton();
+            StartServer();
             axWindowsMediaPlayer1.Focus();
+        }
+
+        private void clearButton()
+        {
+            t1.Text = "";
+            t2.Text = "";
+            t3.Text = "";
+            t4.Text = "";
+            t5.Text = "";
+            t6.Text = "";
+            t7.Text = "";
+
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                //t7.Text += "Server Starting ..." + Environment.NewLine;
+                MessageBox.Show("Server Starting");
+                var ip = System.Net.IPAddress.Parse(ipAddress);
+                server.Start(ip, Convert.ToInt32(portNumber));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            string message = e.MessageString.Trim((char)server.Delimiter);
+            string[] parts = message.Split(new char[] { ':' }, 2);
+
+            if (parts.Length == 2)
+            {
+                string target = parts[0].Trim();
+                string content = parts[1].Trim();
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    switch (target)
+                    {
+                        case "Birth":
+                            t1.Text = "";
+                            t1.Text = content;
+                            break;
+                        case "Death":
+                            t2.Text = "";
+                            t2.Text = content;
+                            break;
+                        case "Marriage":
+                            t3.Text = "";
+                            t3.Text = content;
+                            break;
+                        case "CTC or Negative":
+                            t4.Text = "";
+                            t4.Text = content;
+                            break;
+                        case "Court":
+                            t5.Text = "";
+                            t5.Text = content;
+                            break;
+                        case "Legitimation, Endorsements, etc":
+                            t6.Text = "";
+                            t6.Text = content;
+                            break;
+                        case "Correction":
+                            t7.Text = "";
+                            t7.Text = content;
+                            break;
+                        //default:
+                        //    t7.Text = "";
+                        //    t7.Text = ($"Unknown target: {target}");
+                        //    break;
+                    }
+                });
+
+                e.ReplyLine($"Message received for {target}: {content}");
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    t7.AppendText("Invalid message format: " + message + Environment.NewLine);
+                });
+
+                e.ReplyLine("Invalid message format.");
+            }
         }
 
         private void t1_TextChanged(object sender, EventArgs e)
@@ -41,7 +143,7 @@ namespace Queuing_System
             SpeechSynthesizer _ss = new SpeechSynthesizer();
             axWindowsMediaPlayer1.Ctlcontrols.pause();
             for (int i = 1; i<3; i++) { 
-            string text = $" {x2}! Please Proceed to {x1}";
+            string text = $"Client Number {x2}! Please Proceed to {x1}";
             _ss.Speak(text);
             }
             axWindowsMediaPlayer1.Ctlcontrols.play();
@@ -96,5 +198,6 @@ namespace Queuing_System
             string numberText = typeText.Text;
             callNumber(tableName, numberText);
         }
+
     }
 }
